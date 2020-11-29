7187a9ee140558dc6f5862b9c209f5e18352c8fb
Cariad Eccleston <cariad@cariad.me>
2020-11-29 16:01:19 +0000

    Cleanup (#2)

--- publish.sh
+++ publish.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -e
-aws s3 cp aws-website.cf.yml s3://cariad/aws-website.cf.yml
+
+version="${1:?}"
+local_path=website.cf.yml
+
+sed -i "s/__VERSION__/${version:?}/" "${local_path:?}"
+
+s3_path="s3://cariad/aws-website"
+s3_latest="${s3_path:?}/latest.cf.yml"
+
+aws s3 cp "${local_path:?}" "${s3_latest:?}"
+aws s3 cp "${s3_latest:?}"  "${s3_path:?}/${version:?}.cf.yml"
