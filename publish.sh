2edd53016f837e1179ad21a2ebb98feff173ca6b
Cariad Eccleston <cariad@cariad.me>
2020-11-29 15:59:44 +0000

    Fix version path

--- publish.sh
+++ publish.sh
@@ -9,4 +9,4 @@ s3_path="s3://cariad/aws-website"
 s3_latest="${s3_path:?}/latest.cf.yml"
 
 aws s3 cp "${local_path:?}" "${s3_latest:?}"
-aws s3 cp "${s3_latest:?}"  "${s3_path:?}/${version:?}/latest.cf.yml"
+aws s3 cp "${s3_latest:?}"  "${s3_path:?}/${version:?}.cf.yml"
