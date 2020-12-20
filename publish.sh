435b975191855add3e86f31fb8927bc42984f61a
Cariad Eccleston <cariad@hey.com>
2020-12-20 09:21:18 +0000

    Website (#3)

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 version="${1:?}"
-local_path=website.cf.yml
+local_path=cloudformation/stack.cf.yml
 
 sed -i "s/__VERSION__/${version:?}/" "${local_path:?}"
 
-s3_path="s3://cariad/aws-website"
+s3_path="s3://releases.sitestack.cloud"
 s3_latest="${s3_path:?}/latest.cf.yml"
 
 aws s3 cp "${local_path:?}" "${s3_latest:?}"
